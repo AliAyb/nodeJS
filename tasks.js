05be876f3f48a93b8d773c9bbaf26378ec7112b4
Ali <alii.aybb@gmail.com>
2020-10-20 17:37:32 +0300

    help command

--- tasks.js
+++ tasks.js
@@ -40,6 +40,9 @@ function onDataReceived(text) {
   else if(text === 'hello\n'){
     hello();
   }
+  else if(text === 'help\n'){
+    help();
+  }
   else{
     unknownCommand(text);
   }
@@ -77,6 +80,11 @@ function quit(){
   console.log('Quitting now, goodbye!')
   process.exit();
 }
+
+function help(){
+  console.log('\nquit\nhello\nunknownCommand')
+}
+
 
 // The following line starts the application
 startApp("Ali Ayoub")
